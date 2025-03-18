d0a99dd29644aaef2d535bc098539cef5bfc2987
wa1id <wa1id@users.noreply.github.com>
2025-03-18 12:34:49 +0100

    Update page.tsx
    comment metadata title and description

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import { Metadata } from "next";
 
 import RamadanMarketLandingPage from "@/components/ramadan-market-landing-page";
 
-export const metadata: Metadata = {
-  title: "Coming Soon",
-  description: "Onze website komt binnenkort. Blijf op de hoogte!",
-};
+// export const metadata: Metadata = {
+//   title: "Coming Soon",
+//   description: "Onze website komt binnenkort. Blijf op de hoogte!",
+// };
 
 export default function Home() {
   return <RamadanMarketLandingPage />;
