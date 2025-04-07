688033da42ab12156912fe4c57cb4fc24e1b0c05
Wa1iD <wyachou95@gmail.com>
2025-04-07 14:30:41 +0200

    feat: update homepage and footer with new metadata and contact page component

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,31 +2,32 @@ import { Button } from "./ui/button";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
-  
+
   return (
-    <footer className="border-t border-gray-200 py-6 text-center text-sm text-secondary">
+    <footer className="relative z-10 border-t border-gray-200/30 bg-white py-6 text-center text-sm text-secondary">
       <div className="container mx-auto px-4">
         <p>
-          © {currentYear} ElHikma - DeWijsheid - 2060 vzw. Alle rechten voorbehouden.
+          © {currentYear} ElHikma - DeWijsheid - 2060 vzw. Alle rechten
+          voorbehouden.
         </p>
         <Button
-                variant="link"
-                className="text-secondary underline"
-                data-tally-open="mOkWlg"
-                data-tally-layout="modal"
-                data-tally-width="500"
-                data-tally-emoji-text="👋"
-                data-tally-emoji-animation="wave"
-                data-tally-auto-close="0"
-                data-umami-event="volunteer-signup-button"
-              >
-                Contacteer ons
-              </Button>
+          variant="link"
+          className="text-secondary underline"
+          data-tally-open="mOkWlg"
+          data-tally-layout="modal"
+          data-tally-width="500"
+          data-tally-emoji-text="👋"
+          data-tally-emoji-animation="wave"
+          data-tally-auto-close="0"
+          data-umami-event="volunteer-signup-button"
+        >
+          Contacteer ons
+        </Button>
         <p className="mt-1">
           Website gemaakt door{" "}
-          <a 
-            href="https://wystudio.be" 
-            target="_blank" 
+          <a
+            href="https://wystudio.be"
+            target="_blank"
             rel="noopener noreferrer"
             className="text-primary hover:underline"
           >
@@ -38,4 +39,4 @@ const Footer = () => {
   );
 };
 
-export default Footer; 
+export default Footer;
